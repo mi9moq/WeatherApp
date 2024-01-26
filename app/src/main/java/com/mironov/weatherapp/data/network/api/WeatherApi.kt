3666a13a72104821b5aae70ada1cd2872ff6537f
Denis Mironov <denis5918@gmail.com>
2024-01-26 16:47:45 +0700

    пофиксил неправильный запрос для текущей погоды

--- app/src/main/java/com/mironov/weatherapp/data/network/api/WeatherApi.kt
+++ app/src/main/java/com/mironov/weatherapp/data/network/api/WeatherApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("search.json")
+    @GET("current.json")
     suspend fun loadCurrentWeather(
         @Query("q") query: String,
     ): CurrentWeatherResponse
